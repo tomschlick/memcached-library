c4394e83910d1f549bb135ff8ea66fe9e176c034
Haso Keric <hasokeric@comcast.net>
2011-04-17 00:41:34 -0400

    Using compression type to avoid 64-bit issue http://pecl.php.net/bugs/bug.php?id=18567

--- config/memcached.php
+++ config/memcached.php
@@ -7,11 +7,13 @@ if (!defined('BASEPATH')) exit('No direct script access allowed');
 // --------------------------------------------------------------------------
 $memcached['servers'] = array(
 
-'server-1' 	=> array(
+'default' 	=> array(
+
 					'host'			=> 'localhost',
 					'port'			=> '11211',
 					'weight'		=> '1',
-					'persistent'	=> FALSE,
+					'persistent'	=> FALSE
+					
 					),
 );
 
@@ -20,10 +22,10 @@ $memcached['servers'] = array(
 // --------------------------------------------------------------------------
 $memcached['config'] = array(
 
-	'prefix' 			=> '',			// Prefixes every key value (useful for multi environment setups)
-	'compression'		=> TRUE,		// Turns value compression on/off
-	'expiration'		=> 3600,		// Default content expiration value (in seconds)
-	'delete_expiration'	=> 0,			// Default time between the delete command and the actual delete action occurs (in seconds) 
+	'prefix' 			=> '',						// Prefixes every key value (useful for multi environment setups)
+	'compression'		=> MEMCACHE_COMPRESSED,		// Default: MEMCACHE_COMPRESSED Compression Method or FALSE
+	'expiration'		=> 3600,					// Default content expiration value (in seconds)
+	'delete_expiration'	=> 0						// Default time between the delete command and the actual delete action occurs (in seconds) 
 	
 );
 
